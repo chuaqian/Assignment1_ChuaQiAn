4ec89cfb9dc508ebb69b392dc8904eacc6d921d3
Qi An <S10258309@connect.np.edu.sg>
2023-11-30 22:51:36 +0800

    Comments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    /* Array of background images */
     const backgrounds = [
         "BTS-drops-their-Japanese-single-Stay-Gold-from-Map-Of-The-Soul-7-The-Journey-album.jpg",
         "img2.jpg",
@@ -6,12 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
         "img4.jpg",
         "img5.jpg"
     ];
-
+/* Set variable */
     let currentIndex = 0;
     const backgroundContainer = document.getElementById("background-container");
 
 
-
+/* Ensure index stays within the bounds of the array */
     function changeBackground() {
         currentIndex = (currentIndex + 1) % backgrounds.length;
         const imageUrl = backgrounds[currentIndex];
@@ -19,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
         
         const img = new Image();
         img.src = imageUrl;
-        img.className = "background-image";
+        img.className = "background-image"; /* Setting of background */
 
-        
+        /* For background to change and append */
         const previousImage = backgroundContainer.querySelector(".background-image");
         if (previousImage) {
             backgroundContainer.removeChild(previousImage);
@@ -31,6 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
         backgroundContainer.appendChild(img);
     }
     setInterval(changeBackground, 5000);
-    changeBackground();
+    changeBackground(); /*Interval */
 });
 
